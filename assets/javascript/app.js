fbe118f84d4c03a4946c9a1db6580743085ef896
Simmigon <simmigon.flagg@gmail.com>
2018-08-09 21:00:16 -0400

    This was the easy one?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,87 @@
-// STOPWATCH ACTIVITY (SOLUTION)
-// =============================
+var answer = []
+var answer_key = [];
+var correct = 0;
+var count = 0;
+var incorrect = 0;
+
+function loadresult() {
+  stopwatch.stop();
+
+  $("#game").css("display", "none");
+  $('input[name="Answers"]:checked').each(function () {
+    // alert("Key " + answer_key + " === " + this.value + " Checked ");
+    answer.push(this.value)
+  });
+
+  $.each(answer, function (index, value) {
+    console.log(index + " " + answer_key[index] + " === " + value);
+    if (answer_key[index] === value) {
+      correct++;
+    } else {
+      incorrect++;
+    }
+  });
+  // console.log("Correct " + correct + " " + " Incorrect " + incorrect);
+  $("#grade").css("display", "none");
+  $("#clock").css("display", "none");
+  $("#results").text(`Correct  ${correct} Incorrect ${incorrect}`);
+
+}
+
 
 window.onload = function () {
   $("#start").on("click", stopwatch.start);
+  $("#grade").click(function () {
 
+  loadresult();
 
-
+  });
 
 };
+
+
 var TriviaGame = {
   questions: [
-    {
-      question: "How do crickets hear?",
-      choices: ["Through their wings", "Through their belly", "Through their knees ", "Through their tongue"],
-      answer: "Through their knees"
-    },
-    {
-      question: "Which American city invented plastic vomit?",
-      choices: ["Chicago", "Detroit", "Columbus", "Baltimore"],
-      answer: "Chicago"
-    },
-    {
-      question: "In ‘Ben Hur’, which modern thing can be seen during the chariot scene?",
-      choices: ["A waitress", "A car", "A postbox", "A street lamp"],
-      answer: "A car"
-    },
-    {
-      question: "What was Karl Marx’s favorite color?",
-      choices: ["Brown", "Blue", "Red", "Purple"],
-      answer: "Red"
-    },
-    {
-      question: "What’s the best way to stop crying while peeling onions?",
-      choices: ["Lick almonds", "Suck lemons", "Eat cheese", "Chew gum"],
-      answer: "Chew gum"
-    },
-    {
-      question: "How old was the youngest Pope?",
-      choices: ["11", "17", "22", "29"],
-      answer: "11"
-    },
-    {
-      question: "Which animal sleeps for only five minutes a day?",
-      choices: ["A chameleon", "A koala", "A giraffe", "A beaver"],
-      answer: "A giraffe"
-    },
-    {
-      question: "How many words in the English language end in “dous”?",
-      choices: ["Two", "Four", "Six", "Eight"],
-      answer: "Four"
-    },
+    // {
+    //   question: "How do crickets hear?",
+    //   choices: ["Through their wings", "Through their belly", "Through their knees ", "Through their tongue"],
+    //   answer: "Through their knees"
+    // },
+    // {
+    //   question: "Which American city invented plastic vomit?",
+    //   choices: ["Chicago", "Detroit", "Columbus", "Baltimore"],
+    //   answer: "Chicago"
+    // },
+    // {
+    //   question: "In ‘Ben Hur’, which modern thing can be seen during the chariot scene?",
+    //   choices: ["A waitress", "A car", "A postbox", "A street lamp"],
+    //   answer: "A car"
+    // },
+    // {
+    //   question: "What was Karl Marx’s favorite color?",
+    //   choices: ["Brown", "Blue", "Red", "Purple"],
+    //   answer: "Red"
+    // },
+    // {
+    //   question: "What’s the best way to stop crying while peeling onions?",
+    //   choices: ["Lick almonds", "Suck lemons", "Eat cheese", "Chew gum"],
+    //   answer: "Chew gum"
+    // },
+    // {
+    //   question: "How old was the youngest Pope?",
+    //   choices: ["11", "17", "22", "29"],
+    //   answer: "11"
+    // },
+    // {
+    //   question: "Which animal sleeps for only five minutes a day?",
+    //   choices: ["A chameleon", "A koala", "A giraffe", "A beaver"],
+    //   answer: "A giraffe"
+    // },
+    // {
+    //   question: "How many words in the English language end in “dous”?",
+    //   choices: ["Two", "Four", "Six", "Eight"],
+    //   answer: "Four"
+    // },
     {
       question: "One human hair can support how many kilograms?",
       choices: ["Three", "Five", "Seven", "Nine"],
@@ -71,7 +103,7 @@ var clockRunning = false;
 // Our stopwatch object
 var stopwatch = {
 
-  time: 30,
+  time: 2,
   lap: 1,
 
   reset: function () {
@@ -90,11 +122,12 @@ var stopwatch = {
       $("#buttons").css("display", "block");
       $("#clock").css("display", "block");
       $("#game").css("display", "block");
-      $("#forms").css("display", "block");
+      $("#results").css("display", "block");
+
 
       questions = TriviaGame.questions;
       for (var i = 0; i < questions.length; i++) {
-        console.log("Question : " + (i + 1) + " " + questions[i].question);
+
         var a = $("<div>");
         a.addClass("question-class");
         a.attr("data-name", i + 1);
@@ -103,30 +136,32 @@ var stopwatch = {
         $("#game").append(a);
 
         var form = $("<form>");
-
+        form.attr("id", (i + 1));
         form.attr("data-group", i + 1);
+        form.attr("required");
 
         $("#game").append(form);
 
         for (var j = 0; j < choose.length; j++) {
 
           var input = $('<input>');
-          input.addClass("question");
+          // input.addClass("question");
           input.attr("Type", 'radio');
-          $("#game").append(input);
+          input.attr("value", choose[j]);
+          input.attr("name", "Answers");
+          $("#" + (i + 1)).append(input);
 
           var label = $('<label>');
           // label.addClass("question");
-          // label.attr("data-name", i + 1);
-          label.text(choose[j]);
-          $("#game").append(label);
-
-          console.log("Choices: " + (j + 1) + " " + choose[j])
-
+          label.attr("for", i + 1);
+          label.text("" + choose[j]);
+          $("#" + (i + 1)).append(label);
+          $("#" + (i + 1)).append("<br>");
         }
-        console.log("Answer: " + questions[i].answer);
+        answer_key.push(questions[i].answer);
       }
     }
+    $("#end-of-game").css("display", "block");
   },
   stop: function () {
 
@@ -145,18 +180,13 @@ var stopwatch = {
       $("#onebutton").css("display", "none");
       $("#buttons").css("display", "none");
       $("#clock").css("display", "none");
-      $("#end-of-game").css("display", "block");
       $("#game").css("display", "none");
       $("#forms").css("display", "none");
-
-
-
+      loadresult();
     }
     // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
     //       and save the result in a variable.
     var converted = stopwatch.timeConverter(stopwatch.time);
-
-
     // DONE: Use the variable we just created to show the converted time in the "display" div.
     $("#clock").text(converted);
   },
